9badc9c20b59b51bd07365b4e75c8f8b3ebbde3d
IMarnild <arnild.private@gmail.com>
2021-10-11 12:42:27 +0200

    HistorgramLock

--- Assignment4/Exercise7/week07exercises/app/src/main/java/exercises07/HistogramLock.java
+++ Assignment4/Exercise7/week07exercises/app/src/main/java/exercises07/HistogramLock.java
@@ -1,31 +1,37 @@
 package exercises07;
 
-public class HistogramLock {
-    final private int[] counts;
-    volatile private int total = 0;
-  
-    public HistogramLock(int span) {
-      this.counts = new int[span];
-    }
-  
-    public synchronized void increment(int bin) {
-      counts[bin] = counts[bin] + 1;
-      total++;
-    }
-  
-    public synchronized int getCount(int bin) {
-      return counts[bin];
-    }
-  
-    public float getPercentage(int bin) {
-      return getCount(bin) / getTotal() * 100;
-    }
-  
-    public int getSpan() {
-      return counts.length;
-    }
-  
-    public int getTotal() {
-      return total;
-    }
+public class HistogramLock implements Histogram {
+  final private int[] bins;
+  volatile private int total = 0;
+
+  public HistogramLock(int span) {
+    this.bins = new int[span];
+  }
+
+  public synchronized void increment(int bin) {
+    bins[bin] = bins[bin] + 1;
+    total++;
+  }
+
+  public synchronized int getCount(int bin) {
+    return bins[bin];
+  }
+
+  public float getPercentage(int bin) {
+    return getCount(bin) / getTotal() * 100;
+  }
+
+  public int getSpan() {
+    return bins.length;
+  }
+
+  public int getTotal() {
+    return total;
+  }
+
+  public synchronized int getAndClear(int bin) {
+    int val = bins[bin];
+    bins[bin] = 0;
+    return val;
+  }
 }
