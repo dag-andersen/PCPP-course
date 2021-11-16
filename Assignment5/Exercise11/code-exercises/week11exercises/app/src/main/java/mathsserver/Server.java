57e3e5df1727dccb39ab9c0b58585ed72e70c4f4
marn <marn@siteimprove.com>
2021-11-16 15:46:25 +0100

    week11

--- Assignment5/Exercise11/code-exercises/week11exercises/app/src/main/java/mathsserver/Server.java
+++ Assignment5/Exercise11/code-exercises/week11exercises/app/src/main/java/mathsserver/Server.java
@@ -11,6 +11,7 @@ import akka.actor.typed.ChildFailed;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Server extends AbstractBehavior<Server.ServerCommand> {
@@ -37,11 +38,6 @@ public class Server extends AbstractBehavior<Server.ServerCommand> {
 	}
 
 	/* --- State ---------------------------------------- */
-	HashMap<ActorRef<Worker.WorkerCommand>, Task> workers = new HashMap<>();
-	ArrayList<TaskAndClient> pendingTasks = new ArrayList<>();
-	int minWorkers;
-	int maxWorkers;
-
 	private class TaskAndClient {
 		Task task;
 		ActorRef<Client.ClientCommand> client;
@@ -52,12 +48,31 @@ public class Server extends AbstractBehavior<Server.ServerCommand> {
 		}
 	}
 
+	HashMap<ActorRef<Worker.WorkerCommand>, Task> activeWorkers;
+	LinkedList<ActorRef<Worker.WorkerCommand>> idleWorkers;
+	LinkedList<TaskAndClient> pendingTasks; 
+	int minWorkers;
+	int maxWorkers;
+
 	/* --- Constructor ---------------------------------- */
 	private Server(ActorContext<ServerCommand> context, int minWorkers, int maxWorkers) {
 		super(context);
+		this.activeWorkers = new HashMap<>();
+		this.idleWorkers = new LinkedList<>();
+		this.pendingTasks = new LinkedList<>();
 		this.minWorkers = minWorkers;
 		this.maxWorkers = maxWorkers;
-		fillMinWorkers();
+
+		while(idleWorkers.size() < minWorkers) {
+			var worker = spawnWorker("worker-" + (idleWorkers.size() + activeWorkers.size()));
+			idleWorkers.add(worker);
+		}
+	}
+
+	private ActorRef<Worker.WorkerCommand> spawnWorker(String name) { 
+		ActorRef<Worker.WorkerCommand> worker = getContext().spawn(Worker.create(getContext().getSelf()), name);
+		getContext().watch(worker);
+		return worker;
 	}
 
 	/* --- Actor initial state -------------------------- */
@@ -71,67 +86,57 @@ public class Server extends AbstractBehavior<Server.ServerCommand> {
 		return newReceiveBuilder().onMessage(ComputeTasks.class, this::onComputeTasks)
 				.onMessage(WorkDone.class, this::onWorkDone)
 				.onSignal(ChildFailed.class, this::onChildFailed)
-				.onSignal(Terminated.class, this::onTerminated)
-				// To be extended
 				.build();
 	}
 
 	/* --- Handlers ------------------------------------- */
+
+	public Behavior<ServerCommand> onChildFailed(ChildFailed msg) {
+		// The message contains the information of the actor that crashed
+		// (it is automatically added by akka, and can be accessed with `getRef()`)
+		ActorRef<Void> crashedChild = msg.getRef();
+		// Retreive the task the crashed worker was checking
+		Task nonProcessedTask = activeWorkers.remove(crashedChild);
+
+		var worker = spawnWorker(msg.getRef().path().name());
+		idleWorkers.add(worker);
+
+		getContext().getLog().info("{}: Worker {} crashed trying to compute {} due to {}\nNew worker {} spawned and added to idleWorkers.",
+		getContext().getSelf().path().name(),
+		crashedChild.path().name(),nonProcessedTask, msg.cause(),
+		worker.path().name());
+
+		return this;	
+	}
+
 	public Behavior<ServerCommand> onComputeTasks(ComputeTasks msg) {
 		for (var task : msg.tasks) {
-			var worker = GetFreeWorker(getContext());
-			if (worker != null) {
+			if (idleWorkers.size() > 0) {
+				var worker = idleWorkers.pop();
 				worker.tell(new Worker.ComputeTask(task, msg.client));
+				activeWorkers.put(worker, task);
 			} else {
-				pendingTasks.add(new TaskAndClient(task, msg.client));
+				if (activeWorkers.size() < maxWorkers) {
+					var worker = spawnWorker("worker-" + (idleWorkers.size() + activeWorkers.size()));
+					worker.tell(new Worker.ComputeTask(task, msg.client));
+					activeWorkers.put(worker, task);
+				} else {
+					pendingTasks.add(new TaskAndClient(task, msg.client));
+				}
 			}
 		}
 		return this;
 	}
 
 	public Behavior<ServerCommand> onWorkDone(WorkDone msg) {
-
-		var taskAndClient = pendingTasks.remove(0);
-
-		if (taskAndClient != null) {
+		activeWorkers.remove(msg.worker);
+		if (pendingTasks.size() > 0) {
+			var taskAndClient = pendingTasks.pop();
 			msg.worker.tell(new Worker.ComputeTask(taskAndClient.task, taskAndClient.client));
+			activeWorkers.put(msg.worker, taskAndClient.task);
 		} else {
-			workers.remove(msg.worker);
-			fillMinWorkers();
+			idleWorkers.add(msg.worker);
 		}
 		return this;
-	}
-
-	public Behavior<ServerCommand> onChildFailed(ChildFailed msg) {
-		fillMinWorkers();
-		return this;
-	}
-
-	public Behavior<ServerCommand> onTerminated(Terminated msg) {
-		// Do nothing
-		return this;
-	}
-
-	/* --- Helpers -------------------------------------- */
-	public ActorRef<Worker.WorkerCommand> GetFreeWorker(ActorContext<ServerCommand> context) {
-		var x = workers.entrySet().stream().filter(entry -> entry.getValue() == null).findFirst();
-		if (x.isPresent())
-			return x.get().getKey();
-		else if (workers.size() < maxWorkers) {
-			ActorRef<Worker.WorkerCommand> worker = context.spawn(Worker.create(context.getSelf()),
-					"worker-" + workers.size());
-			workers.put(worker, null);
-			return worker;
-		}
-		return null;
-	}
-
-	public void fillMinWorkers() {
-		getContext().getLog().info("{}: Filling idle workers", getContext().getSelf().path().name());
-		while (workers.size() < minWorkers) {
-			ActorRef<Worker.WorkerCommand> worker = getContext().spawn(Worker.create(getContext().getSelf()),
-					"worker-" + workers.size());
-			workers.put(worker, null);
-		}
 	}
 }
