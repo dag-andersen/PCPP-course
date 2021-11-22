13c58d1e8c9b34c2391c07618fb5e9898eddf402
IMarnild <arnild.private@gmail.com>
2021-11-22 14:13:58 +0100

    exercise 1

--- Assignment6/week12/code-exercises/week12exercises/app/src/test/java/testingconcurrency/ConcurrentSetTest.java
+++ Assignment6/week12/code-exercises/week12exercises/app/src/test/java/testingconcurrency/ConcurrentSetTest.java
@@ -1,14 +1,21 @@
 package testingconcurrency;
 
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import org.junit.jupiter.api.BeforeEach;
-// TODO: Very likely you need to expand the list of imports
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.concurrent.BrokenBarrierException;
 
 public class ConcurrentSetTest {
 
     // Variable with set under test
     private ConcurrentIntegerSet set;
-
-    // TODO: Very likely you should add more variables here
+    private CyclicBarrier barrier;
+    private final static ExecutorService pool = Executors.newCachedThreadPool();
+    private static final int nrThreads = 8;
 
     // Uncomment the appropriate line below to choose the class to
     // test
@@ -21,5 +28,34 @@ public class ConcurrentSetTest {
         // set = new ConcurrentIntegerSetLibrary();
     }
 
-    // TODO: Define your tests below
+    @RepeatedTest(10)
+    @DisplayName("Exercise_01_Add")
+    public void exercise_01_Add() {
+        barrier = new CyclicBarrier(nrThreads + 1);
+        var N = 3;
+
+        for (int i = 0; i < nrThreads; i++) {
+            pool.execute(() -> {
+                try {
+                    barrier.await();
+                    for (int j = 0; j < N; j++) {
+                        int size = set.size();
+                        set.add(size);
+                    }
+                    barrier.await();
+                } catch (Exception e) {
+                    System.out.println("failed to add");
+                    e.printStackTrace();
+                }
+            });
+        }
+        try {
+            barrier.await();
+            barrier.await();
+        } catch (InterruptedException | BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+
+        assertTrue(N * nrThreads == set.size(), "set.size() == " + set.size() + ", but we expected " + N * nrThreads);
+    }
 }
